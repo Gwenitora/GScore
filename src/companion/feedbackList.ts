b5550b3a7d4fb1af23fd58c202377601a269c62f
Gwenitora <gwenitora.gardu74@gmail.com>
2025-03-08 14:48:16 +0100

    reinit for gitignore

--- src/companion/feedbackList.ts
+++
@@ -1,9 +0,0 @@
-import FeedbackManager from "../managers/feedbackManager.js";
-import inGettingMode from "./feedbacks/InGettingMode.js";
-
-const setupFeedbacks = () => {
-    FeedbackManager
-        .addFeedback(new inGettingMode());
-}
-
-export default setupFeedbacks;
--- src/companion/presets/Categories.ts
+++
@@ -1,6 +0,0 @@
-enum CompPresetCategories {
-    Scores = 'Scores',
-    Keys = 'Virtuals Keys'
-}
-
-export default CompPresetCategories;
